bf38511d0fabc86e5b19343627e829650a2059ce
Madhav Malhotra <47190635+Madhav-Malhotra@users.noreply.github.com>
2026-06-10 20:39:33 +0200

    Add: kth-largest-integer-in-a-stream - submission-4

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-4.py
@@ -0,0 +1,34 @@
+'''
+Constraints: 
+- Can have duplicate elements
+- kth largest element in sorted order, not numerically
+- Stream always has at least k elements. 
+
+Thinking: 
+- We don't have all elements upfront to construct a static array. Heap is ideal
+- We don't want to pop k times to find the kth largest element. 
+    - Instead, maintain a heap with just the top k elements. 
+    - Maintaining a min heap would give us quick access to the smallest of the top k elements (the kth largest)
+'''
+
+import heapq
+
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.h = nums[:k]
+        self.k = k
+        heapq.heapify(self.h)
+
+        # Add remaining nums if not small
+        for n in nums[k:]:
+            if n > self.h[0]:
+                heapq.heapreplace(self.h, n)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.h, val)
+        if len(self.h) > self.k:
+            heapq.heappop(self.h)
+        
+        return self.h[0]
+        
